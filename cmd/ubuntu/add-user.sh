1cf3e17596a4ddade29ed29358e036c56e0ec656
Xing Xing <mikespook@gmail.com>
2014-11-13 11:23:25 +0800

    reconcile adduser process

--- cmd/ubuntu/add-user.sh
+++ cmd/ubuntu/add-user.sh
@@ -1,51 +1,62 @@
 #add new user
 
-USER=mikespook
-PASSWORD=false
+user=mikespook
+password=false
 
 usage() {
-    echo "Usage: $0 -h [-u $USER] [-p]"
-	echo " -h Host of SSH key"
-	echo " -u User name to be created"
+    echo "Usage: $0 [-k \$path] [-u \$user] [-p]"
+	echo " -k Path of SSH public key"
+	echo " -u User to be created"
 	echo " -p Initializing password"
-	exit 3
 }
 
-while getopts 'h:u:p' o &>> /dev/null; do
+while getopts 'k:u:p' o &>> /dev/null; do
     case "$o" in
-    h)
-        HOST="$OPTARG";;
+    k)
+        key="$OPTARG";;
 	u)
-		USER="$OPTARG";;
+		user="$OPTARG";;
     p)
-        PASSWORD=true;;
+        password=true;;
     *)
-        usage;;
+        usage
+		exit 3;;
     esac
 done
 
 check_root
 
-id $USER &>> /dev/null
+id $user &>> /dev/null
 [ $? -eq 0 ] && echo "User $USER already existed." && exit 2
 
-echo "Adding user $USER ..."
+printf "\n\tSSH Pub-key:\t$key\n"
+printf "\tUser name:\t$user:\n"
+printf "\tInit passwd:\t$password\n\n"
+echo -n "Continue to add? [y|N]: "
+read answer
+if [ "$answer" != "Y" ] && [ "$answer" != "y" ]; then
+	exit 1
+fi
 
-adduser --quiet --disabled-password --gecos "" $USER
+adduser --quiet --disabled-password --gecos "" $user
 egrep -i "^admin" /etc/group
 [ $? -ne 0 ] && addgroup --system admin
-usermod -a -G admin $USER
+usermod -a -G admin $user
+usermod -a -G sudo $user
 
-echo "Setting SSH key ..."
-home=`getent passwd "$USER" | cut -d: -f6`
-if [ ! -z "$HOST" ]; then
+home=`getent passwd "$user" | cut -d: -f6`
+if [ ! -z "$key" ]; then
 	mkdir -p $home/.ssh
-	scp $HOST:~/.ssh/authorized_keys $HOME/.ssh/
-	chown -R $USER:$USER $home/.ssh
+	if [ -f $key ]; then
+		cp $key $home/.ssh/authorized_keys
+	else
+		wget $key -O $home/.ssh/authorized_keys
+	fi
+	chown -R $user:$user $home/.ssh
 	chmod 700 $home/.ssh
 	chmod 600 $home/.ssh/authorized_keys
 fi
 
-$PASSWORD && passwd $USER
+$password && passwd $user
 
 exit 0
